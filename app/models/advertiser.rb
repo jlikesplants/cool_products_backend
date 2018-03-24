6b7f3b70d38127eadfc3fde6688cd862e3ab21c3
Justine Winnie <justine.winnie@gettyimages.com>
2018-03-24 11:00:42 -0700

    Add uniqueness constraint to ad model

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -1,5 +1,5 @@
 class Advertiser < ApplicationRecord
   has_many :skus
   has_many :products, through: :skus
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
