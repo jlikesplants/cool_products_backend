ee0734cd27db157dbc953226c12fee9cb4a99089
Justine Winnie <justine.winnie@gettyimages.com>
2018-03-24 11:28:00 -0700

    Bring in smarter_csv gem and refactor to use

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'rack-cors', :require => 'rack/cors'
 
 gem 'activerecord-import'
 
+gem 'smarter_csv'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- lib/tools/seed_importer.rb
+++ lib/tools/seed_importer.rb
@@ -1,54 +1,53 @@
 class SeedImporter 
   require 'csv'
   require 'activerecord-import'
+  require 'smarter_csv'
   
   def initialize(file)
     @file = file 
   end
-    
+  
+  def read_csv
+    SmarterCSV.process(@file)
+  end
+  
+  
   def import_csv
+    csv_data = read_csv
+      
     products = []
     advertisers = []
-    row_data = []
-    
-    start = Time.now
-    
-    CSV.foreach(@file, headers: true) do |row|      
-      product_name = row["Product Name"]
-      advertiser_name = row["Advertiser"]
-      sku = row["SKU"]
-      
-      print "Reading CSV row: #{row}"
-
-      row_data << row
-      
-      product = Product.new(name: product_name)
+    csv_data.each do |data|
+      product = Product.new(name: data[:product_name])
       products << product
-
-      advertiser = Advertiser.new(name: advertiser_name)
-      advertisers << advertiser
       
+      advertiser = Advertiser.new(name: data[:advertiser])
+      advertisers << advertiser
     end
     
+    puts "Importing products..."
     Product.import(products)
+    
+    puts "Importing advertisers..."
     Advertiser.import(advertisers)
     
-    import_skus(row_data)
-    
-    finished = Time.now
-    puts "All done.... it took #{finished - start}"
+    import_skus(csv_data)
   end
   
   def import_skus(row_data)
+    start = Time.now
     skus = []
     row_data.each do |row|
-      print "Sku seeding row: #{row}"
-      product = Product.find_by(name: row["Product Name"])
-      advertiser = Advertiser.find_by(name: row["Advertiser"])
+      product = Product.find_by(name: row[:product_name])
+      advertiser = Advertiser.find_by(name: row[:advertiser])
       
-      skus << Sku.new(advertiser: advertiser, product: product, sku: row["SKU"])
+      skus << Sku.new(advertiser: advertiser, product: product, sku: row[:sku])
     end
     
+    puts "Importing skus..."
     Sku.import(skus)
+    finished = Time.now
+    puts "All done.... it took #{finished - start}"
   end
 end
+
