2c500b3ebf1fb0e7f83dacc8f1aef9a49772b8b8
Justine Winnie <justine.winnie@gettyimages.com>
2018-03-24 16:13:50 -0700

    Refactor to use class methods and faster seeding

--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -7,7 +7,6 @@ namespace :import do
   desc 'Read products from CSV'
   task :csv, [:file] => :environment do |t, args|
     file = args[:file]
-    importer = SeedImporter.new(file)
-    importer.import_csv
+    SeedImporter.import_csv(file)
   end
 end
--- lib/tools/seed_importer.rb
+++ lib/tools/seed_importer.rb
@@ -2,27 +2,34 @@ class SeedImporter
   require 'csv'
   require 'activerecord-import'
   require 'smarter_csv'
-  
-  def initialize(file)
-    @file = file 
+
+  def self.read_csv(file)
+    SmarterCSV.process(file)
   end
   
-  def read_csv
-    SmarterCSV.process(@file)
-  end
-  
-  
-  def import_csv
-    csv_data = read_csv
+  def self.import_csv(file)
+    start = Time.now
+    csv_data = read_csv(file)
       
+    product_names = []
     products = []
+    
+    advertiser_names = []
     advertisers = []
+    
     csv_data.each do |data|
-      product = Product.new(name: data[:product_name])
-      products << product
       
-      advertiser = Advertiser.new(name: data[:advertiser])
-      advertisers << advertiser
+      if !product_names.include?(data[:product_name])
+        product = Product.new(name: data[:product_name])
+        products << product
+        product_names << data[:product_name]
+      end
+      
+      if !advertiser_names.include?(data[:advertiser])
+        advertiser = Advertiser.new(name: data[:advertiser])
+        advertisers << advertiser
+        advertiser_names << data[:advertiser]
+      end
     end
     
     puts "Importing products..."
@@ -31,23 +38,22 @@ class SeedImporter
     puts "Importing advertisers..."
     Advertiser.import(advertisers)
     
+    puts "Importing skus..."
     import_skus(csv_data)
+    
+    finished = Time.now
+    puts "All done.... it took #{finished - start}"
   end
   
-  def import_skus(row_data)
-    start = Time.now
+  def self.import_skus(row_data)
     skus = []
     row_data.each do |row|
       product = Product.find_by(name: row[:product_name])
       advertiser = Advertiser.find_by(name: row[:advertiser])
-      
       skus << Sku.new(advertiser: advertiser, product: product, sku: row[:sku])
     end
     
-    puts "Importing skus..."
     Sku.import(skus)
-    finished = Time.now
-    puts "All done.... it took #{finished - start}"
   end
 end
 
